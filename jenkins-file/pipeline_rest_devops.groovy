66372e4b772e5a2208d538b92dbb00365403df64
djvasquezn <djvasquezn@gmail.com>
2019-06-13 20:12:10 -0500

    Update pipeline_rest_devops.groovy

--- jenkins-file/pipeline_rest_devops.groovy
+++ jenkins-file/pipeline_rest_devops.groovy
@@ -1,38 +1,35 @@
-pipeline {
-	agent any
-    tools {
-        maven 'maven-local'
-    }
-    stages {
-        stage('Checkout') {
-			steps {
-				git url: 'https://github.com/djvasquezn/rest-devops.git' 
-				}
-			}
+node {
+ 
+    withMaven(maven:'maven-local') {
+ 
+       stage('Checkout') {
+            git url: 'https://github.com/piomin/sample-spring-microservices.git'
+        }
+ 
  
         stage('Build') {
-			steps {
-				sh 'mvn clean install'
-	 
-				def pom = readMavenPom file:'pom.xml'
-				print pom.version
-				env.version = pom.version
-			}
+            sh 'mvn clean install'
+ 
+            def pom = readMavenPom file:'pom.xml'
+            print pom.version
+            env.version = pom.version
         }
  
         stage('Image') {
-			steps {
-				dir ('account-service') {
-					def app = docker.build "localhost:5000/account-service:${env.version}"
-					app.push()
-				}
-			}
+            dir ('discovery-service') {
+                def app = docker.build "localhost:5000/discovery-service:${env.version}"
+                app.push()
+            }
         }
  
         stage ('Run') {
-			steps {
-				docker.image("localhost:5000/account-service:${env.version}").run('-p 2222:2222 -h account --name account --link discovery')}
-			}
+            docker.image("localhost:5000/discovery-service:${env.version}").run('-p 8761:8761 -h discovery --name discovery')
+        }
+ 
+        stage ('Final') {
+            build job: 'account-service-pipeline', wait: false
+        }      
  
     }
+ 
 }
